f45ebbb176403403c19bf12d5d5d9e0bcdeeab0a
Mixer1993 <94074361+Mixer1993@users.noreply.github.com>
2022-02-01 00:01:22 +0200

    Add files via upload

--- diplom project 2/script/script.js
+++ diplom project 2/script/script.js
@@ -1,1 +1,15 @@
 $('.footer-paralax').parallax({imageSrc: 'https://www.publicdomainpictures.net/pictures/150000/velka/sunset-sky-1455125487HWs.jpg'});
+
+$('.about-items > div').on('click', function(){
+    $(this).children('.about-icon').toggleClass('active');
+    $(this).parent('.about-items').toggleClass('toggleBg');
+    $(this).toggleClass('toggleColor');
+    $(this).next('p').slideToggle(300)
+})
+
+$('.easily_find-items > div').on('click', function(){
+    $(this).next('p').slideToggle(1100);
+    $(this).parent('.easily_find-items').toggleClass('easily_find-items_padding');
+    
+    
+})
